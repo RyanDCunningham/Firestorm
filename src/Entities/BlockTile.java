d940ecacdae9266b334185c544af3833e70b1886
Justin Cunningham <cunninghamj14826@live.bucks.edu>
2017-02-06 20:45:09 -0500

    push block collision finsish Ryan rules Justin drools

--- src/Entities/BlockTile.java
+++ src/Entities/BlockTile.java
@@ -6,6 +6,7 @@ import Entities.Entity;
 import Entities.Mob;
 import States.GameState;
 import Textures.Sprite;
+import World.Tile;
 
 
 public class BlockTile extends Mob {
@@ -26,14 +27,15 @@ public class BlockTile extends Mob {
 	//mobs = new ArrayList<Mob>();
 	//Mob m : mobs;
 	
+	
 	public void move() {
-		if(hasHorizontalCollision()) x += dx;
-		if(hasVerticalCollision()) y += dy;
-		if(isMovingHorizontal()) x += dx;
-		if(isMovingVertical()) y += dy;
+		//if(hasHorizontalCollision()) x += dx;
+		//if(hasVerticalCollision()) y += dy;
+		if(isPushingHorizontal() && !hasHorizontalCollision()) x += dx;
+		if(isPushingVertical() && !hasVerticalCollision()) y += dy;
 	}
 	
-	protected boolean isMovingVertical() {
+	protected boolean isPushingVertical() {
 		for(int i = 0; i < state.getEntity().size(); i++) {
 			Entity e = state.getEntity().get(i);
 			if(getBounds().intersects(e.getTop())) {
@@ -51,7 +53,7 @@ public class BlockTile extends Mob {
 		return false;
 	}
 	
-	protected boolean isMovingHorizontal() {
+	protected boolean isPushingHorizontal() {
 		for(int i = 0; i < state.getEntity().size(); i++) {
 			Entity e = state.getEntity().get(i);
 			if(getBounds().intersects(e.getRight())) {
@@ -68,5 +70,41 @@ public class BlockTile extends Mob {
 		
 		return false;
 	}	
+	
+	protected boolean hasVerticalCollision() {
+		for(int i = 0; i < state.getTiles().size(); i++) {
+			Tile t = state.getTiles().get(i);
+			if(getBounds().intersects(t.getTop()) && dy > 0) {
+				dy = 0;
+				//System.out.println("Down");
+				return true;
+			}
+			if(getBounds().intersects(t.getBottom()) && dy < 0) {
+				dy = 0;
+				//System.out.println("Up");
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	protected boolean hasHorizontalCollision() {
+		for(int i = 0; i < state.getTiles().size(); i++) {
+			Tile t = state.getTiles().get(i);
+			if(getBounds().intersects(t.getRight()) && dx < 0) {
+				dx = -1;
+				//System.out.println("Left");
+				return true;
+			}
+			if(getBounds().intersects(t.getLeft()) && dx > 0) {
+				dx = -1;
+				//System.out.println("Right");
+				return true;
+			}	
+		}
+		
+		return false;
+	}
 
 }
--- src/Entities/Mob.java
+++ src/Entities/Mob.java
@@ -18,8 +18,8 @@ public abstract class Mob extends Entity {
 	}
 
 	public void move() {
-		if(!hasHorizontalCollision()) x += dx;
-		if(!hasVerticalCollision()) y += dy;
+		if(!hasHorizontalCollision() && !isPushingHorizontal()) x += dx;
+		if(!hasVerticalCollision() && !isPushingVertical()) y += dy;
 	}
 	
 	protected boolean hasVerticalCollision() {
@@ -44,12 +44,12 @@ public abstract class Mob extends Entity {
 		for(int i = 0; i < state.getTiles().size(); i++) {
 			Tile t = state.getTiles().get(i);
 			if(getBounds().intersects(t.getRight()) && dx < 0) {
-				dx = -1;
+				dx = 0;
 				//System.out.println("Left");
 				return true;
 			}
 			if(getBounds().intersects(t.getLeft()) && dx > 0) {
-				dx = -1;
+				dx = 0;
 				//System.out.println("Right");
 				return true;
 			}	
@@ -57,5 +57,41 @@ public abstract class Mob extends Entity {
 		
 		return false;
 	}
+	
+	protected boolean isPushingVertical() {
+		for(int i = 0; i < state.getEntity().size(); i++) {
+			Entity e = state.getEntity().get(i);
+			if(getBounds().intersects(e.getTop()) && dy > 0) {
+				dy = 0;
+				System.out.println("Down");
+				return true;
+			}
+			if(getBounds().intersects(e.getBottom()) && dy < 0) {
+				dy = 0;
+				System.out.println("Up");
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	protected boolean isPushingHorizontal() {
+		for(int i = 0; i < state.getEntity().size(); i++) {
+			Entity e = state.getEntity().get(i);
+			if(getBounds().intersects(e.getRight()) && dx < 0) {
+				dx = 0;
+				System.out.println("Left");
+				return true;
+			}
+			if(getBounds().intersects(e.getLeft()) && dx > 0) {
+				dx = 0;
+				System.out.println("Right");
+				return true;
+			}	
+		}
+		
+		return false;
+	}	
 	
 }
